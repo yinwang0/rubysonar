ec1921fe13d6b7b72ab1fb5df517177f06e1537c
Yin Wang <yinwang0@gmail.com>
2014-01-02 17:10:00 -0800

    go back to old ways temporarily

--- src/main/java/org/yinwang/rubysonar/ast/BinOp.java
+++ src/main/java/org/yinwang/rubysonar/ast/BinOp.java
@@ -1,11 +1,7 @@
 package org.yinwang.rubysonar.ast;
 
 import org.jetbrains.annotations.NotNull;
-import org.yinwang.rubysonar.Analyzer;
-import org.yinwang.rubysonar.Binding;
 import org.yinwang.rubysonar.State;
-import org.yinwang.rubysonar.types.BoolType;
-import org.yinwang.rubysonar.types.IntType;
 import org.yinwang.rubysonar.types.Type;
 
 
@@ -31,162 +27,16 @@ public class BinOp extends Node {
     @NotNull
     @Override
     public Type transform(State s) {
-
         Type ltype = transformExpr(left, s);
-        Type rtype;
+        Type rtype = transformExpr(right, s);
 
-        // boolean operations
-        if (op == Op.And) {
-            if (ltype.isUndecidedBool()) {
-                rtype = transformExpr(right, ltype.asBool().s1);
-            } else {
-                rtype = transformExpr(right, s);
-            }
-
-            if (ltype.isTrue() && rtype.isTrue()) {
-                return Type.TRUE;
-            } else if (ltype.isFalse() || rtype.isFalse()) {
-                return Type.FALSE;
-            } else if (ltype.isUndecidedBool() && rtype.isUndecidedBool()) {
-                State falseState = State.merge(ltype.asBool().s2, rtype.asBool().s2);
-                return new BoolType(rtype.asBool().s1, falseState);
-            } else {
-                return Type.BOOL;
-            }
-        }
-
-        if (op == Op.Or) {
-            if (ltype.isUndecidedBool()) {
-                rtype = transformExpr(right, ltype.asBool().s2);
-            } else {
-                rtype = transformExpr(right, s);
-            }
-
-            if (ltype.isTrue() || rtype.isTrue()) {
-                return Type.TRUE;
-            } else if (ltype.isFalse() && rtype.isFalse()) {
-                return Type.FALSE;
-            } else if (ltype.isUndecidedBool() && rtype.isUndecidedBool()) {
-                State trueState = State.merge(ltype.asBool().s1, rtype.asBool().s1);
-                return new BoolType(trueState, rtype.asBool().s2);
-            } else {
-                return Type.BOOL;
-            }
-        }
-
-        rtype = transformExpr(right, s);
-
-        if (ltype.isUnknownType() || rtype.isUnknownType()) {
+        if (ltype != Type.UNKNOWN) {
+            return ltype;
+        } else if (rtype != Type.UNKNOWN) {
+            return rtype;
+        } else {
             return Type.UNKNOWN;
         }
-
-        // Don't do specific things about string types at the moment
-        if (ltype == Type.STR && rtype == Type.STR) {
-            return Type.STR;
-        }
-
-        // try to figure out actual result
-        if (ltype.isIntType() && rtype.isIntType()) {
-            IntType leftNum = ltype.asIntType();
-            IntType rightNum = rtype.asIntType();
-
-            if (op == Op.Add) {
-                return IntType.add(leftNum, rightNum);
-            }
-
-            if (op == Op.Sub) {
-                return IntType.sub(leftNum, rightNum);
-            }
-
-            if (op == Op.Mul) {
-                return IntType.mul(leftNum, rightNum);
-            }
-
-            if (op == Op.Div) {
-                return IntType.div(leftNum, rightNum);
-            }
-
-            // comparison
-            if (op == Op.Lt || op == Op.Gt) {
-                Node leftNode = left;
-                IntType trueType, falseType;
-                Op op1 = op;
-
-                if (!left.isName()) {
-                    leftNode = right;
-
-                    IntType tmpNum = rightNum;
-                    rightNum = leftNum;
-                    leftNum = tmpNum;
-
-                    op1 = Op.invert(op1);
-                }
-
-                if (op1 == Op.Lt) {
-                    if (leftNum.lt(rightNum)) {
-                        return Type.TRUE;
-                    } else if (leftNum.gt(rightNum) || leftNum.eq(rightNum)) {
-                        return Type.FALSE;
-                    } else {
-                        // transfer bound information
-                        State s1 = s.copy();
-                        State s2 = s.copy();
-
-                        if (leftNode.isName()) {
-                            // true branch: if l < r, then l's upper bound is r's upper bound
-                            trueType = new IntType(leftNum);
-                            trueType.setUpper(rightNum.upper);
-
-                            // false branch: if l > r, then l's lower bound is r's lower bound
-                            falseType = new IntType(leftNum);
-                            falseType.setLower(rightNum.lower);
-                            String id = leftNode.asName().id;
-
-                            for (Binding b : s.lookup(id)) {
-                                Node loc = b.node;
-                                s1.update(id, new Binding(loc, trueType, b.kind));
-                                s2.update(id, new Binding(loc, falseType, b.kind));
-                            }
-                        }
-                        return new BoolType(s1, s2);
-                    }
-                }
-
-                if (op1 == Op.Gt) {
-                    if (leftNum.gt(rightNum)) {
-                        return Type.TRUE;
-                    } else if (leftNum.lt(rightNum) || leftNum.eq(rightNum)) {
-                        return Type.FALSE;
-                    } else {
-                        // undecided, need to transfer bound information
-                        State s1 = s.copy();
-                        State s2 = s.copy();
-
-                        if (leftNode.isName()) {
-                            // true branch: if l > r, then l's lower bound is r's lower bound
-                            trueType = new IntType(leftNum);
-                            trueType.setLower(rightNum.lower);
-
-                            // false branch: if l < r, then l's upper bound is r's upper bound
-                            falseType = new IntType(leftNum);
-                            falseType.setUpper(rightNum.upper);
-                            String id = leftNode.asName().id;
-
-                            for (Binding b : s.lookup(id)) {
-                                Node loc = b.node;
-                                s1.update(id, new Binding(loc, trueType, b.kind));
-                                s2.update(id, new Binding(loc, falseType, b.kind));
-                            }
-                        }
-                        return new BoolType(s1, s2);
-                    }
-                }
-            }
-        }
-
-
-        Analyzer.self.putProblem(this, "operator " + op + " cannot be applied on operands " + ltype + " and " + rtype);
-        return ltype;
     }
 
 
@@ -195,6 +45,5 @@ public class BinOp extends Node {
     public String toString() {
         return "(" + left + " " + op + " " + right + ")";
     }
-
 
 }
