2c7d189f77064194c03b0fe896eebc8b14c369a4
Yin Wang <yinwang0@gmail.com>
2014-01-08 22:19:33 -0800

    emit self-ref for bindings

--- src/main/java/org/yinwang/rubysonar/JSONDump.java
+++ src/main/java/org/yinwang/rubysonar/JSONDump.java
@@ -256,6 +256,7 @@ public class JSONDump {
                     }
                 }
             }
+            writeRefJson(b.node, b, refJson);
         }
 
         for (JsonGenerator json : allJson) {
