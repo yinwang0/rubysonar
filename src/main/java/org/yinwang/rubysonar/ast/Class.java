2691dad04b4afa793bcdb5d54e47ce6b4e5b544d
Yin Wang <yinwang0@gmail.com>
2014-02-27 17:49:53 -0800

    set static context for "class << self" when self is a module

--- src/main/java/org/yinwang/rubysonar/ast/Class.java
+++ src/main/java/org/yinwang/rubysonar/ast/Class.java
@@ -4,6 +4,7 @@ import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 import org.yinwang.rubysonar.*;
 import org.yinwang.rubysonar.types.ClassType;
+import org.yinwang.rubysonar.types.ModuleType;
 import org.yinwang.rubysonar.types.Type;
 
 
@@ -55,7 +56,7 @@ public class Class extends Node {
     public Type transform(@NotNull State s) {
         if (locator != null) {
             Type existing = transformExpr(locator, s);
-            if (existing instanceof ClassType) {
+            if (existing instanceof ClassType || existing instanceof ModuleType) {
                 if (body != null) {
                     boolean wasStatic = Analyzer.self.staticContext;
                     Analyzer.self.setStaticContext(true);
